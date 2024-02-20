34dbeb137bba32b071dcf59b9ee3d1cac10c2b10
Vaibhav Bergal <bergalvaibhav33@gmail.com>
2024-02-20 20:47:12 +0530

    Add sticky Navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -34,89 +34,90 @@ function Header() {
 
   return (
     <>
-      <div className="flex items-center h-16 gap-3 px-3 text-white bg-black shadow-lg">
-        <Button
-          onClick={toggleNavbar}
-          className="p-0 px-1 text-3xl bg-transparent md:hidden hover:bg-black"
-        >
-          {!isOpen ? <HiMenuAlt1 /> : <FiX />}
-        </Button>
-        <div className="flex items-center justify-between w-full md:justify-around">
-          <Link to="/" className="text-2xl font-medium ">
-            <span className="text-amber-400">Dev</span>ify
-          </Link>
-          <ul className="flex gap-2 md:gap-6">
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                `py-4 hidden md:block hover:text-blue-500 ${
-                  isActive
-                    ? "scale-x-110 font-medium text-blue-500"
-                    : "text-white"
-                }`
-              }
-            >
-              Home
-            </NavLink>
-            {!authStatus ? (
+      <div className="sticky top-0 z-10 ">
+        <div className="flex items-center h-16 gap-3 px-3 text-white bg-black shadow-lg ">
+          <Button
+            onClick={toggleNavbar}
+            className="p-0 px-1 text-3xl bg-transparent md:hidden hover:bg-black"
+          >
+            {!isOpen ? <HiMenuAlt1 /> : <FiX />}
+          </Button>
+          <div className="flex items-center justify-between w-full md:justify-around">
+            <Link to="/" className="text-2xl font-medium ">
+              <span className="text-amber-400">Dev</span>ify
+            </Link>
+            <ul className="flex gap-2 md:gap-6">
               <NavLink
-                to="/login"
+                to="/"
                 className={({ isActive }) =>
-                  `pt-3 hover:text-blue-500 ${
+                  `py-4 hidden md:block hover:text-blue-500 ${
                     isActive
                       ? "scale-x-110 font-medium text-blue-500"
-                      : "text-white/80"
+                      : "text-white"
                   }`
                 }
               >
-                <RiAccountCircleFill className="text-3xl" />
+                Home
               </NavLink>
-            ) : null}
-            {authStatus && (
-              <li className="pt-2 text-3xl">
-                <LogoutBtn />
-              </li>
-            )}
+              {!authStatus ? (
+                <NavLink
+                  to="/login"
+                  className={({ isActive }) =>
+                    `pt-3 hover:text-blue-500 ${
+                      isActive
+                        ? "scale-x-110 font-medium text-blue-500"
+                        : "text-white/80"
+                    }`
+                  }
+                >
+                  <RiAccountCircleFill className="text-3xl" />
+                </NavLink>
+              ) : null}
+              {authStatus && (
+                <li className="pt-2 text-3xl">
+                  <LogoutBtn />
+                </li>
+              )}
 
-            <NavLink
-              to="/cart"
-              className={({ isActive }) =>
-                `py-3 hover:text-blue-500 relative px-1 ${
-                  isActive
-                    ? "scale-x-110 font-medium text-amber-300"
-                    : "text-white"
-                }`
-              }
-            >
-              <PiShoppingCartSimpleBold className="text-3xl " />
-              <span
-                className={`absolute text-white top-1 right-1 px-[6px] py-[2px] text-[10px] ${
-                  cart.length > 0 ? "bg-orange-500" : ""
-                } rounded-full`}
+              <NavLink
+                to="/cart"
+                className={({ isActive }) =>
+                  `py-3 hover:text-blue-500 relative px-1 ${
+                    isActive
+                      ? "scale-x-110 font-medium text-amber-300"
+                      : "text-white"
+                  }`
+                }
               >
-                {cart.length > 0 ? cart.length : ""}
-              </span>
-            </NavLink>
+                <PiShoppingCartSimpleBold className="text-3xl " />
+                <span
+                  className={`absolute text-white top-1 right-1 px-[6px] py-[2px] text-[10px] ${
+                    cart.length > 0 ? "bg-orange-500" : ""
+                  } rounded-full`}
+                >
+                  {cart.length > 0 ? cart.length : ""}
+                </span>
+              </NavLink>
+            </ul>
+          </div>
+        </div>
+        <div
+          className={`${
+            isOpen ? "block" : "hidden"
+          } md:hidden border-t border-b shadow-inner transition-all ease-in-out border-white/20 py-3 mb-3   bg-neutral-900`}
+        >
+          <ul className="text-center text-white capitalize font-mooli">
+            {categories.map((category) => (
+              <li
+                key={category}
+                className="py-2 cursor-pointer hover:text-amber-400"
+                onClick={() => handleCategory(category)}
+              >
+                {category}
+              </li>
+            ))}
           </ul>
         </div>
-      </div>
-
-      <div
-        className={`${
-          isOpen ? "block" : "hidden"
-        } md:hidden border-t border-b shadow-inner transition-all ease-in-out border-white/20 py-3 mb-3  bg-black/90`}
-      >
-        <ul className="text-center text-white capitalize font-mooli">
-          {categories.map((category) => (
-            <li
-              key={category}
-              className="py-2 cursor-pointer hover:text-amber-400"
-              onClick={() => handleCategory(category)}
-            >
-              {category}
-            </li>
-          ))}
-        </ul>
       </div>
       <ul className="justify-end hidden gap-2 py-3 text-lg capitalize md:flex font-mooli">
         {categories.map((category) => (
